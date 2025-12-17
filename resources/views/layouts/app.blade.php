364def4fbfbd3cf412f3dd64f51dbfe563834a18
Nama Kamu <s22310158@student.unklab.ac.id>
2025-12-17 16:19:57 +0800

    struktur website

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <h1>UFO - Sistem Informasi Organisasi Mahasiswa UNKLAB</h1>
+    <hr>
+
+    {{-- TEMPAT SEMUA HALAMAN MASUK --}}
+    @yield('content')
+
+</body>
+</html>
---
+++ resources/views/mahasiswa/beranda.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>UFO - Beranda Mahasiswa</title>
+</head>
+<body>
+
+<h1>UFO - Sistem Informasi Organisasi Mahasiswa UNKLAB</h1>
+
+<p>Selamat datang mahasiswa Universitas Klabat.</p>
+
+<ul>
+    <li><a href="/organisasi">Daftar Organisasi</a></li>
+    <li><a href="/kegiatan">Kegiatan Organisasi</a></li>
+    <li><a href="/login">Login Pengurus</a></li>
+</ul>
+
+</body>
+</html>
