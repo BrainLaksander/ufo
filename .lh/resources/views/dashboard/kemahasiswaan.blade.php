ac1b59ebc9e672f1ee8f7e6eb32d3f55452e570a
Nama Kamu <s22310158@student.unklab.ac.id>
2026-01-25 23:52:59 +0800

    Update auth, dashboard, dan middleware

---
+++ .lh/resources/views/dashboard/kemahasiswaan.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.kemahasiswaan')
+
+@section('title', 'Dashboard Kemahasiswaan')
+
+@section('content')
+<h2>Dashboard Departemen Kemahasiswaan</h2>
+
+<ul>
+    <li>Verifikasi proposal kegiatan</li>
+    <li>Monitoring kegiatan organisasi</li>
+    <li>Laporan dan evaluasi</li>
+</ul>
+@endsection
