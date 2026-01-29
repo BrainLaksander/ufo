2569283a9e9b7b57398f0986550c8bc7dc4dc45a
Nama Kamu <s22310158@student.unklab.ac.id>
2026-01-29 22:30:30 +0800

    Remove .history folder and ensure it's ignored

--- .history/resources/views/dashboard/kemahasiswaan.blade_20251222135613.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Dashboard Kemahasiswaan')
-
-@section('content')
-	<h2>Dashboard Kemahasiswaan</h2>
-	<p>Halaman ini menampilkan monitoring dan validasi untuk departemen kemahasiswaan.</p>
-@endsection
--- .history/resources/views/mahasiswa/organisasi.blade_20251222135614.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Daftar Organisasi')
-
-@section('content')
-	<h2>Daftar Organisasi</h2>
-	<p>Halaman daftar organisasi akan menampilkan organisasi mahasiswa yang terdaftar.</p>
-@endsection
