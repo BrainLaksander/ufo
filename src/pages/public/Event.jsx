b36df24bcb41df512e6daf6dccfc8cf80ccfc50e
BrainLaksander <s22310158@student.unklab.ac.id>
2026-04-08 02:02:49 +0000

    chore(cleanup): remove legacy react-vite stack and old tooling files

--- src/pages/public/Event.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Event() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold">Event</h1>
-      <p>Halaman event placeholder.</p>
-    </div>
-  );
-}
